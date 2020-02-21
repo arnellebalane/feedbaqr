4f902e2afe85432f98597ab68898edcf14790187
Arnelle Balane <arnellebalane@gmail.com>
2020-02-21 22:45:28 +0800

    Render subject ID QR code

--- src/components/subject-info.js
+++ src/components/subject-info.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import QRCode from 'qrcode.react';
 
 const SubjectInfo = ({ subject }) => {
   return (
     <header>
       <h1>{subject.title}</h1>
       <p>{subject.description}</p>
+
+      <QRCode value={subject.id} />
     </header>
   );
 };
