5ddd489897605f374dfa0795c53baaaa5ea50d52
Arnelle Balane <arnellebalane@gmail.com>
2020-02-21 23:21:31 +0800

    Use full site URL in QR code instead of just the subject ID

--- src/components/subject-info.js
+++ src/components/subject-info.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import QRCode from 'qrcode.react';
 
+const BASE_URL = process.env.BASE_URL || 'http://localhost:8000/';
+
 const SubjectInfo = ({ subject }) => {
+  const url = `${BASE_URL}subject/?id=${subject.id}`;
+
   return (
     <header>
       <h1>{subject.title}</h1>
       <p>{subject.description}</p>
 
-      <QRCode value={subject.id} size={1000} />
+      <QRCode value={url} size={1000} />
     </header>
   );
 };
