ec1575934cfa46d6b7ef89a3c68614b626f0ceed
Arnelle Balane <arnellebalane@gmail.com>
2020-02-21 21:20:30 +0800

    Initialize Firebase project in lib/firebase.js

---
+++ src/lib/firebase.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase';
+import 'firebase/firestore';
+import 'firebase/storage';
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyBXewH-sBCdokkUVKqmgnFpNB72Jesoqu4',
+  authDomain: 'feedbaqr.firebaseapp.com',
+  databaseURL: 'https://feedbaqr.firebaseio.com',
+  projectId: 'feedbaqr',
+  storageBucket: 'feedbaqr.appspot.com',
+  messagingSenderId: '900927958030',
+  appId: '1:900927958030:web:cf48a72c686cc3d229a05f',
+  measurementId: 'G-T9H3Z3DV63',
+});
+
+export const firestore = firebase.firestore();
+export const storage = firebase.storage();
